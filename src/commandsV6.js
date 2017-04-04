ade41fabb10530edc0e937de40aa29bf0893c0b1
marcus <WittigMarcus@gmail.com>
2017-04-04 08:20:06 +0200

    fixed fullColor.rgb() using wrong color circle, issue #18

--- src/commandsV6.js
+++ src/commandsV6.js
@@ -239,7 +239,7 @@ RgbFullColorCommand.prototype.rgb = function(zone, r, g, b) {
   var hsv=helper.rgbToHsv(r, g, b);
   var hue = (hsv[0] == 0)?0xB0:hsv[0] * 0xFF % 0x167;
   return [
-    this.hue(zone, hue),
+    this.hue(zone, hue, true),
     this.saturation(zone, hsv[1], true),
     this.brightness(zone, hsv[2])
   ]
