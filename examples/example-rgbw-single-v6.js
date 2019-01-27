c713ca207fbc59d82d0d88baa2137dc51198fed7
marcus <WittigMarcus@gmail.com>
2019-01-27 14:20:28 +0100

    example code snippet for v6 RGBWW single zone, issue #56

---
+++ examples/example-rgbw-single-v6.js
@@ -0,0 +1,31 @@
+var Milight = require('../src/index');
+var commands = Milight.commandsV6;
+
+// Important Notes:
+// *  Instead of providing the global broadcast address which is the default, you should provide the IP address
+//    of the Milight Controller for unicast mode. Don't use the global broadcast address on Windows as this may give
+//    unexpected results. On Windows, global broadcast packets will only be routed via the first network adapter. If
+//    you want to use a broadcast address though, use a network-specific address, e.g. for `192.168.0.1/24` use
+//    `192.168.0.255`.
+// *  Note, for the V6 command set each command must be provided with a zone parameter as shown in the example below!
+
+var light = new Milight.MilightController({
+    ip: "255.255.255.255",
+    type: 'v6'
+  });
+
+light.ready().then(function() {
+  light.sendCommands(commands.rgbwSingle.on(), commands.rgbwSingle.brightness(100));
+  light.pause(1000);
+
+  light.sendCommands(commands.rgbwSingle.off());
+  light.pause(1000);
+  
+  light.close().then(function () {
+    console.log("All command have been executed - closing Milight");
+  });
+  console.log("Invocation of asynchronous Milight commands done");
+}).catch(function(error) {
+  console.log(error);
+  light.close();
+});
